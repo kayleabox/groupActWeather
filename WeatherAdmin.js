00cef5ff0db3958a19ba9c5f165f4d4789d9293c
kaylea <kayleabox09@gmail.com>
2017-06-24 11:49:58 -0500

    working on inquirer

--- WeatherAdmin.js
+++ WeatherAdmin.js
@@ -1,8 +1,20 @@
 var weather = require("weather-js");
+var inquirer = require("inquirer");
 
 var fs = require("fs");
 
 var WeatherAdmin = function(){
+    this.again = function(){
+        inquirer.prompt([{
+            type:"confirm",
+            name:"again",
+            message:"wanna go again?"
+        }]).then(function(answers){
+            if (answers.again){
+                promptUser();
+            }
+        })
+    };
     this.getData = function(){
         fs.readFile("log.txt", "utf-8", function(err, data){
             if(err){
@@ -10,6 +22,7 @@ var WeatherAdmin = function(){
             }
             console.log(data);
         })
+        this.again();
     };
     this.newSearch = function(name, place){
         weather.find({search: place, degreeType: 'F'}, function(err, result) {
@@ -17,10 +30,12 @@ var WeatherAdmin = function(){
             console.log(JSON.stringify(result, null, 2));
             var logInfo = name+": "+place+" "+result[0].current.date+" \n";
             fs.appendFile("log.txt", logInfo); 
+            this.again();
         });
     };
 
 }
+
 
 
  module.exports=WeatherAdmin;  
--- cli.js
+++ cli.js
@@ -1,8 +1,46 @@
 var WeatherAdmin = require("./WeatherAdmin");
-
-var userType = process.argv[2];
+var inquirer = require("inquirer");
 
 var MyAdmin = new WeatherAdmin();
+
+function promptUser(){
+    inquirer.prompt([{
+        type:"list",
+        name:"userType",
+        message:"what type of user are you?",
+        choices:["user", "admin"]
+    }]).then(function(answers){
+        if(answers.userType === "user"){
+
+            inquirer.prompt([
+                {
+                    type:"input",
+                    name:"name",
+                    message:"what is your name?"
+                },
+                {
+                    type:"input",
+                    name:"place",
+                    message:"where do you want the weather?"
+                }
+            ]).then(function(answers){
+                MyAdmin.newSearch(answers.name, answers.place);
+                //again();
+            })
+
+
+        }
+        else if(answers.userType === "admin"){
+            MyAdmin.getData();
+            //again();
+        }
+        
+    })
+}
+
+promptUser();
+
+/*var userType = process.argv[2];
 
 if(userType === "user"){
     var name = process.argv[3];
@@ -12,4 +50,4 @@ if(userType === "user"){
 }
 else if(userType === "admin"){
     MyAdmin.getData();
-}
+}*/
